e9748d8a63dbfc6288c5978fecb409b9407101dc
jzero92@daum.net <jzero92@daum.net>
2021-05-11 19:33:16 +0900

    quartz Ex

--- src/QuartzTest/domain/HelloJob.java
+++ src/QuartzTest/domain/HelloJob.java
@@ -1,19 +1,45 @@
 package QuartzTest.domain;
 
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+import org.quartz.*;
+import org.quartz.impl.matchers.GroupMatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Date;
+import java.util.HashSet;
 
 public class HelloJob implements Job {
 
-    Logger logger = LoggerFactory.getLogger(HelloJob.class);
+    final Logger logger = LoggerFactory.getLogger(HelloJob.class);
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         logger.info("Hello Job - {}", new Date());
+
+        try {
+            logger.info("{}", context.getJobDetail().getKey());
+            logger.info("{}", Thread.currentThread().getName());
+
+            //context.getJobDetail().getJobDataMap().put("state", "running");
+            context.getScheduler().getJobDetail(context.getJobDetail().getKey()).getJobDataMap().put("state", "running");
+
+            HashSet<JobKey> jobs = (HashSet<JobKey>) context.getScheduler().getJobKeys(GroupMatcher.anyGroup());
+            jobs.stream().forEach(jb -> {
+                try {
+                    JobDetail tmp = context.getScheduler().getJobDetail(jb);
+                    String jobName = tmp.getJobDataMap().get("jobName").toString();
+                    String serverInfo = tmp.getJobDataMap().get("serverInfo").toString();
+                    String state = tmp.getJobDataMap().get("state").toString();
+                    Date startTime = (Date) tmp.getJobDataMap().get("startTime");
+
+                    logger.info("jobName : {}, serverInfo : {}, state : {}, startTime : {}", jobName, serverInfo, state, startTime);
+                } catch (SchedulerException e) {
+                    e.printStackTrace();
+                }
+            });
+            context.getScheduler().getCurrentlyExecutingJobs().stream().forEach(s->{logger.info("cur : {}", s.getJobDetail().getJobDataMap().get("jobName"));});
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/QuartzTest/example01/SimpleExample.java
+++ src/QuartzTest/example01/SimpleExample.java
@@ -1,19 +1,19 @@
 package QuartzTest.example01;
 
 import QuartzTest.domain.HelloJob;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerFactory;
-import org.quartz.Trigger;
+import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Set;
 
+import static org.quartz.DateBuilder.evenSecondDate;
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.TriggerBuilder.newTrigger;
+import static org.quartz.DateBuilder.evenMinuteDate;
 
 
 public class SimpleExample {
@@ -24,19 +24,56 @@ public class SimpleExample {
 
         SchedulerFactory sf = new StdSchedulerFactory();
         Scheduler sched = sf.getScheduler();
+        Scheduler sched2 = sf.getScheduler();
+
+        if (sched == sched2) {
+            logger.info("==");
+        }
+        if (sched.equals(sched2)) {
+            logger.info("equals");
+        }
 
         Calendar cal = Calendar.getInstance();
         cal.set(2021, Calendar.MAY,10,16,51,30);
 
-        Date runTime = cal.getTime();
+        //Date runTime = cal.getTime();
+        Date runTime = evenMinuteDate(new Date());
+        Date runTime2 = evenMinuteDate(new Date());
+        Date runTime3 = evenMinuteDate(new Date());
+        //Date runTime2 = evenSecondDate(runTime);
+        //Date runTime3 = evenSecondDate(runTime2);
 
-        JobDetail job = newJob(HelloJob.class).withIdentity("job1", "group1").build();
+        JobDataMap dataMap = new JobDataMap();
+        dataMap.put("jobName", "job1");
+        dataMap.put("serverInfo", "STG1");
+        dataMap.put("state", "wait");
+        dataMap.put("startTime", runTime);
+
+        JobDataMap dataMap2 = new JobDataMap();
+        dataMap2.put("jobName", "job2");
+        dataMap2.put("serverInfo", "STG1");
+        dataMap2.put("state", "wait");
+        dataMap2.put("startTime", runTime2);
+
+        JobDataMap dataMap3 = new JobDataMap();
+        dataMap3.put("jobName", "job3");
+        dataMap3.put("serverInfo", "STG1");
+        dataMap3.put("state", "wait");
+        dataMap3.put("startTime", runTime3);
+
+        JobDetail job = newJob(HelloJob.class).usingJobData(dataMap).withIdentity("job1", "group1").build();
+        JobDetail job2 = newJob(HelloJob.class).usingJobData(dataMap2).withIdentity("job2", "group1").build();
+        JobDetail job3 = newJob(HelloJob.class).usingJobData(dataMap3).withIdentity("job3", "group1").build();
 
         Trigger trigger = newTrigger().withIdentity("trigger1", "group1").startAt(runTime).build();
+        Trigger trigger2 = newTrigger().withIdentity("trigger2", "group1").startAt(runTime2).build();
+        Trigger trigger3 = newTrigger().withIdentity("trigger3", "group1").startAt(runTime3).build();
 
         sched.scheduleJob(job, trigger);
+        sched.scheduleJob(job2, trigger2);
+        sched.scheduleJob(job3, trigger3);
 
-        logger.info("{} will run at : {}", job.getKey(), runTime);
+        //.info("{} will run at : {}", job.getKey(), runTime);
 
         sched.start();
 
