ba7edd577f922b6de8361a8bffe9c71c0d6dc972
jzero92@daum.net <jzero92@daum.net>
2021-08-25 21:47:13 +0900

    next 7

--- src/DynamicBeat/dynamic_beat/DynamicBeat.java
+++ src/DynamicBeat/dynamic_beat/DynamicBeat.java
@@ -19,16 +19,40 @@ public class DynamicBeat extends JFrame {
     private Graphics screenGraphic;
 
     //private Image introBackground;
-    private Image introBackground = new ImageIcon(dyMain.class.getResource("../../images/intro_background.jpg")).getImage();
+    private Image background = new ImageIcon(dyMain.class.getResource("../../images/intro_background.jpg")).getImage();
+    private Image selectedImage = new ImageIcon(dyMain.class.getResource("../../images/start_img_evening_sky.png")).getImage();
+
+    private Image titleImage = new ImageIcon(dyMain.class.getResource("../../images/evening_sky_title_img.png")).getImage();
+
     private JLabel menuBar = new JLabel(new ImageIcon(dyMain.class.getResource("../../images/menuBar.png")));
 
 
     private ImageIcon exitButtonEnteredImage = new ImageIcon(dyMain.class.getResource("../../images/exit_entered.png"));
     private ImageIcon exitButtonUnEnteredImage = new ImageIcon(dyMain.class.getResource("../../images/exit_unEntered.png"));
 
+    private ImageIcon startButtonUnEnteredImage = new ImageIcon(dyMain.class.getResource("../../images/start_unEntered_btn_img.png"));
+    private ImageIcon startButtonEnteredImage = new ImageIcon(dyMain.class.getResource("../../images/start_Entered_btn_img.png"));
+
+    private ImageIcon stopButtonUnEnteredImage = new ImageIcon(dyMain.class.getResource("../../images/stop_unEntered_btn_img.png"));
+    private ImageIcon stopButtonEnteredImage = new ImageIcon(dyMain.class.getResource("../../images/stop_Entered_btn_img.png"));
+
+    private ImageIcon leftButtonUnEnteredImage = new ImageIcon(dyMain.class.getResource("../../images/left_unEntered_img.png"));
+    private ImageIcon leftButtonEnteredImage = new ImageIcon(dyMain.class.getResource("../../images/left_Entered_img.png"));
+
+    private ImageIcon rightButtonUnEnteredImage = new ImageIcon(dyMain.class.getResource("../../images/right_unEntered_img.png"));
+    private ImageIcon rightButtonEnteredImage = new ImageIcon(dyMain.class.getResource("../../images/right_Entered_img.png"));
+
+
+
     private JButton exitButton = new JButton(exitButtonUnEnteredImage);
+    private JButton startButton = new JButton(startButtonUnEnteredImage);
+    private JButton stopButton = new JButton(stopButtonUnEnteredImage);
+    private JButton leftButton = new JButton(leftButtonUnEnteredImage);
+    private JButton rightButton = new JButton(rightButtonUnEnteredImage);
 
     private int mouseX, mouseY;
+
+    private boolean isMainScreen = false;
 
     public DynamicBeat() {
 
@@ -47,7 +71,7 @@ public class DynamicBeat extends JFrame {
         JPanel panel = new JPanel(){
             public void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                g.drawImage(introBackground, 0, 0, this);
+                g.drawImage(background, 0, 0, this);
             }
         };
 
@@ -96,9 +120,121 @@ public class DynamicBeat extends JFrame {
                 System.exit(0);
             }
         });
-
         panel.add(exitButton);
         //add(exitButton);
+
+        startButton.setBounds(40,200,400,100);
+        startButton.setBorderPainted(false);
+        startButton.setContentAreaFilled(false);
+        startButton.setFocusPainted(false);
+        startButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                startButton.setIcon(startButtonEnteredImage);
+                startButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+                Music buttonEnteredMusic = new Music("mouse_hover.mp3", false);
+                buttonEnteredMusic.start();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                startButton.setIcon(startButtonUnEnteredImage);
+                startButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                startButton.setVisible(false);
+                stopButton.setVisible(false);
+                leftButton.setVisible(true);
+                rightButton.setVisible(true);
+                background = new ImageIcon(dyMain.class.getResource("../../images/main_background.jpg")).getImage();
+                isMainScreen = true;
+            }
+        });
+        panel.add(startButton);
+
+
+        stopButton.setBounds(40,330,400,100);
+        stopButton.setBorderPainted(false);
+        stopButton.setContentAreaFilled(false);
+        stopButton.setFocusPainted(false);
+        stopButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                stopButton.setIcon(stopButtonEnteredImage);
+                stopButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+                Music buttonEnteredMusic = new Music("mouse_hover.mp3", false);
+                buttonEnteredMusic.start();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                stopButton.setIcon(stopButtonUnEnteredImage);
+                stopButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.exit(0);
+            }
+        });
+        panel.add(stopButton);
+
+
+        leftButton.setBounds(500,550,60,60);
+        leftButton.setVisible(false);
+        leftButton.setBorderPainted(false);
+        leftButton.setContentAreaFilled(false);
+        leftButton.setFocusPainted(false);
+        leftButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                leftButton.setIcon(leftButtonEnteredImage);
+                leftButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+                Music buttonEnteredMusic = new Music("mouse_hover.mp3", false);
+                buttonEnteredMusic.start();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                leftButton.setIcon(leftButtonUnEnteredImage);
+                leftButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                //
+            }
+        });
+        panel.add(leftButton);
+
+        rightButton.setBounds(700,550,60,60);
+        rightButton.setVisible(false);
+        rightButton.setBorderPainted(false);
+        rightButton.setContentAreaFilled(false);
+        rightButton.setFocusPainted(false);
+        rightButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                rightButton.setIcon(rightButtonEnteredImage);
+                rightButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+                Music buttonEnteredMusic = new Music("mouse_hover.mp3", false);
+                buttonEnteredMusic.start();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                rightButton.setIcon(rightButtonUnEnteredImage);
+                rightButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                //
+            }
+        });
+        panel.add(rightButton);
 
         setContentPane(panel);
 
@@ -120,8 +256,14 @@ public class DynamicBeat extends JFrame {
 
     public void screenDraw(Graphics g) {
         //logger.info("repaint");
-        g.drawImage(introBackground, 0, 0, null);
+        g.drawImage(background, 0, 0, null);
         super.paintComponents(g);
+
+        if (isMainScreen) {
+            // logger.info("into");
+            g.drawImage(selectedImage, 340, 100, null);
+            g.drawImage(titleImage, 360, 120, null);
+        }
         this.repaint();
 
 
